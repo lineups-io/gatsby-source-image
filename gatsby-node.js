8ec866b8df873a961d71c5a9d70310b84b5f9c34
Lucas Blancas <lablancas@gmail.com>
2018-11-01 00:32:34 -0500

    iterate until all pages have been processed

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,14 +2,17 @@ const connect = require('./client')
 const query = require('./query')
 const { ImageNode } = require('./nodes')
 
-exports.sourceNodes = ({ actions }, { uri, key, account }) => {
+const sourceNodes = ({ actions }, options, { offset = 0, limit = 20 }) => {
+  const { uri, key, account } = options
   const { createNode } = actions
 
   const client = connect(uri, key)
-  const variables = { account, offset: 0, limit: 20 }
+  const variables = { account, offset, limit }
 
   return client.query({ query, variables }).then(({ data }) => {
-    data.pages.items.forEach(page => {
+    const { count, items } = data.pages
+
+    items.forEach(page => {
       page.apartments.items.forEach(apartment => {
         if (apartment.defaultPhoto) {
           createNode(ImageNode({
@@ -19,5 +22,12 @@ exports.sourceNodes = ({ actions }, { uri, key, account }) => {
         }
       })
     })
+
+    const processed = offset + items.length
+    if (count > processed) {
+      return sourceNodes({ actions }, options, { offset: processed, limit })
+    }
   })
 }
+
+exports.sourceNodes = sourceNodes
