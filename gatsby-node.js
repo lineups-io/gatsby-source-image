38efc8d88e5f8422a3cc8923327d872386b76bb1
Lucas Blancas <lablancas@gmail.com>
2018-11-01 00:36:54 -0500

    Revert "iterate until all pages have been processed"
    This reverts commit 8ec866b8df873a961d71c5a9d70310b84b5f9c34.

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,17 +2,14 @@ const connect = require('./client')
 const query = require('./query')
 const { ImageNode } = require('./nodes')
 
-const sourceNodes = ({ actions }, options, { offset = 0, limit = 20 }) => {
-  const { uri, key, account } = options
+exports.sourceNodes = ({ actions }, { uri, key, account }) => {
   const { createNode } = actions
 
   const client = connect(uri, key)
-  const variables = { account, offset, limit }
+  const variables = { account, offset: 0, limit: 20 }
 
   return client.query({ query, variables }).then(({ data }) => {
-    const { count, items } = data.pages
-
-    items.forEach(page => {
+    data.pages.items.forEach(page => {
       page.apartments.items.forEach(apartment => {
         if (apartment.defaultPhoto) {
           createNode(ImageNode({
@@ -22,12 +19,5 @@ const sourceNodes = ({ actions }, options, { offset = 0, limit = 20 }) => {
         }
       })
     })
-
-    const processed = offset + items.length
-    if (count > processed) {
-      return sourceNodes({ actions }, options, { offset: processed, limit })
-    }
   })
 }
-
-exports.sourceNodes = sourceNodes
